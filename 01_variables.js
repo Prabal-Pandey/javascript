4a2d5f8ddcc3b7d6d2efe2c0fe9a85dc5840bdca
Prabal-Pandey <p18abal789@gmail.com>
2026-08-27 18:43:12 +0530

    variables

---
+++ 01_variables.js
@@ -0,0 +1,16 @@
+// console.log("prabal");
+const accountId = 15472
+let accountEmail = "prabal@gmail.com"
+var accountPassword = "12345"
+accountCity = "auraiya"
+
+// accountId = 12345  const does not change
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+/*
+prefer not to use var 
+due to scope problem
+*/
