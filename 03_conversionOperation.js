dcf3e155315ab6d9edda65341c612db26941eb3a
Prabal-Pandey <p18abal789@gmail.com>
2026-08-28 22:22:43 +0530

    operators

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -33,3 +33,35 @@ console.log(typeof(num))
 console.log(num)  //  gives NaN -> not a number
 
 // null -> number gives NaN
+
+
+// ******************* Operations *******************
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello "
+let str2 = "world"
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+// increment and decrement operators
